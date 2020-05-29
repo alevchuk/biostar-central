249469b873e8f0112741d53514ac8bf4896b8c0f
Aleksandr Levchuk <alevchuk@gmail.com>
2020-05-29 16:53:06 +0000

    nginx log dir

--- loadbalancer/biostar.nginx.conf.generate.sh
+++ loadbalancer/biostar.nginx.conf.generate.sh
@@ -5,11 +5,14 @@ set -ue
 # Required environmental variables:
 # * DOMAIN_NAME
 # * LIVE_DIR
+# * LOG_DIR
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cat $DIR/biostar.nginx.conf.TEMPLATE | \
 	sed "s|%DOMAIN_NAME%|$DOMAIN_NAME|g" | \
-	sed "s|%LIVE_DIR%|$LIVE_DIR|g" > $DIR/biostar.nginx.conf
+	sed "s|%LIVE_DIR%|$LIVE_DIR|g" | \
+	sed "s|%LOG_DIR%|$LOG_DIR|g" | \
+	> $DIR/biostar.nginx.conf
 
 echo "Generated $DIR/biostar.nginx.conf"
