4265006818f7225533b220f86d1da04bbe95c207
Aleksandr Levchuk <alevchuk@gmail.com>
2020-05-30 16:37:33 +0000

    Separate nginx configs

--- log/README.md
+++
@@ -1,7 +0,0 @@
-# Dir
-
-This dir will contain logs.
-
-# Logrotate
-
-Make sure to setup logrotate to avoid running out of disk space.
--- nginx/biostar.nginx.conf.generate.sh
+++ nginx/biostar.nginx.conf.generate.sh
@@ -9,10 +9,12 @@ set -ue
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cat $DIR/biostar.nginx.conf.TEMPLATE | \
-	sed "s|%DOMAIN_NAME%|$DOMAIN_NAME|g" | \
-	sed "s|%LIVE_DIR%|$LIVE_DIR|g" | \
-	sed "s|%LOG_DIR%|$LOG_DIR|g" | \
-	> $DIR/biostar.nginx.conf
+for conf in biostar.nginx.conf__certbot biostar.nginx.conf__no_certbot; do
+    cat $DIR/$conf.TEMPLATE | \
+    	sed "s|%DOMAIN_NAME%|$DOMAIN_NAME|g" | \
+    	sed "s|%LIVE_DIR%|$LIVE_DIR|g" | \
+    	sed "s|%LOG_DIR%|$LOG_DIR|g" > ~/$conf
 
-echo "Generated $DIR/biostar.nginx.conf"
+    echo "Generated ~/$conf"
+done
+
