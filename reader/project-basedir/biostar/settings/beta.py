a0d941a1e9fbce6d9fc5839a5afdd25eae416317
Aleksandr Levchuk <alevchuk@gmail.com>
2020-07-12 07:18:59 -0700

    FRIENDLY_PREFIX = "beta.ln.support" in reader just like in writer

--- reader/project-basedir/biostar/settings/beta.py
+++ reader/project-basedir/biostar/settings/beta.py
@@ -2,3 +2,5 @@ from biostar.settings.deploy import *
 
 DATABASES['default']['NAME'] = 'beta'
 DATABASES['default']['USER'] = 'beta_dbro'
+
+FRIENDLY_PREFIX = "beta.ln.support"  # cannot have under-bars (_s)
